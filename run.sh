13c32ab94c57454931a3d4cd3474ad05e318e358
Mauro D'Agostini <mauroalejandro20@gmail.com>
2025-10-20 23:55:58 -0500

    ready for RPI

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to project directory
+cd ~/hermes
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Run the main script
+python main.py
+
+# Deactivate virtual environment
+deactivate
